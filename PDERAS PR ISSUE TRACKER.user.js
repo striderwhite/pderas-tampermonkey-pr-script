19bccf6ebc0f6defea778a5014a052e70360e914
Strider White <strider.white@pderas.com>
2023-02-01 18:53:32 -0700

    init

---
+++ PDERAS PR ISSUE TRACKER.user.js
@@ -0,0 +1,104 @@
+// ==UserScript==
+// @name         PDERAS PR ISSUE TRACKER
+// @namespace    http://pderas.com/
+// @version      0.1
+// @description  Hacks the existing harvest button for tracking time against a PR's underlying issue instead of the PR itself.
+// @author       Strider White <strider@striderwhite.com>
+// @match        https://github.com/*/*/pull/*
+// @icon         none
+// @grant        none
+// ==/UserScript==
+
+(async function() {
+    'use strict';
+
+    let relatedIssue = await getMeAsync("issue-link", 'related issue', true);
+    let harvestButton = await getMeAsync("#partial-discussion-header > div.gh-header-show > div > div > button.harvest-timer.btn.btn-sm.mr-1", 'harvest button');
+    let issueTitle = await getMeAsync("js-issue-title", 'issue title', true);
+
+    if (!relatedIssue) {
+        displayErrorMessage("Unable to find the related issue - did you link it correctly?");
+    }
+
+    if (!harvestButton) {
+        displayErrorMessage("Unable to find the harvest button - is the plugin working?");
+    }
+
+    if (!issueTitle) {
+        displayErrorMessage("Unable to find the issue title - maybe the css selector changed?");
+    }
+
+    if (harvestButton && relatedIssue && issueTitle) {
+        // There can be multiple issues on the page with this class, usually the first one is the correct one
+        relatedIssue = relatedIssue[0];
+        // It appears there should only be one issue title, but maybe not - grab the first one
+        issueTitle = issueTitle[0];
+        console.dir(issueTitle);
+
+        // Grab data wew need from the related issue
+        let relatedIssueData = {
+            url: relatedIssue.href,
+            text: issueTitle.innerText,
+            number: relatedIssue.href.split('/')[relatedIssue.href.split('/').length-1] // splits the URL to get the item #
+        };
+
+        // Modify the JSON payload sent to harvest
+        let harvestDataItem = JSON.parse(harvestButton.dataset.item);
+        harvestDataItem.id = relatedIssueData.number;
+        harvestDataItem.name = `#${relatedIssueData. number}: ${relatedIssueData.text}`;
+        harvestDataItem.permalink = relatedIssueData.url;
+
+        // Changes what gets written into the Iframe window
+        harvestButton.dataset.item = JSON.stringify(harvestDataItem);
+
+        // Changes what URL the issue tracks against in harvest
+        harvestButton.dataset.permalink = relatedIssueData.url;
+
+        // Modify the harvest button so we know it worked
+        harvestButton.innerText = "[PDERAS] TRACK AGAINST ISSUE #" + relatedIssueData.number;
+    }
+
+
+})();
+
+async function getMeAsync(selector, lookingFor, byClassName = false) {
+    const MAX_ATTEMPTS = 10;
+    let ATTEMPTS = 1;
+
+    let found = byClassName ? document.getElementsByClassName(selector) : document.querySelector(selector);
+
+    while (!found) {
+        if (ATTEMPTS++ > MAX_ATTEMPTS) { break; }
+        // "sleep" for 1 second
+        await new Promise(r => setTimeout(r, 1000));
+        console.log('Looking again...');
+        found = byClassName ? document.getElementsByClassName(selector) : document.querySelector(selector);
+    }
+
+    if (!found) {
+        console.error("WARNING UNABLE TO FIND: " + lookingFor);
+    }
+
+    return found;
+}
+
+function displayErrorMessage(message) {
+
+    let actionElm = null;
+
+    try {
+        actionElm = document.getElementsByClassName("gh-header-actions")[0];
+    } catch (e) {
+        console.error(message);
+    }
+
+    if (actionElm) {
+        const warningDiv = document.createElement("div");
+        const warningText = document.createTextNode("Warning: Unable to find the related issue. Did you link it properly?");
+
+        warningDiv.appendChild(warningText);
+        actionElm.appendChild(warningDiv);
+    }
+
+
+}
--- README.md
+++ README.md
@@ -1,1 +1,38 @@
-# pderas-tampermonkey-pr-script
+# PDERAS Tamper Monkey PR Script
+
+When we approve a PR, we must track time in Harvest against the underlying issue the PR refers to, not the PR itself. I did what most sane developers would do and automate this as much as possible becaus 3-4x extra clicks for every PR is going to add up in a busy day, and who enjoys spending time on tedious repetition?
+
+What this does is hijack the harvest button for tracking time against a PR's underlying issue instead of the PR itself. Now when you click the "track time" button, the payload sent to the Harvest servers is modified to track time against the linked issue in the PR, neat.
+
+## Install/Requirements
+
+1) FireFox (Doesn't support Chrome due to limitations I don't understand)
+2) [Tampermonkey Fire Fox plugin](https://addons.mozilla.org/en-CA/firefox/addon/tampermonkey/);
+3) You must add a CSP exclusion rule to Firefox.
+
+Do this by:
+
+- Settings > Privacy & Security > Manage Exceptions
+- Then add https://github.com there
+4) Now add the `PDERAS PR ISSUE TRACKER.user.js` script to Tampermonkey.
+
+Do this by:
+
+
+- Either downloading the file and adding the file Tampermonkey in the settings
+- Or simply create your own script then copy-paste into the script
+5) Activate the script
+6) Now navigate to a PR
+
+## Limitations
+- The PR *MUST* refer to an issue by github linkage - for example by #1234 - the link must be "clickable"
+- This uses the title of the PR to fill in the description of what you track against, so title your PR's accordingly. If you want a different message you'll have to modify it on the Harvest popup window.
+- Relies on specific query selectors - it is fragile
+- Doesn't support Chrome
+- Doesn't automate reviewing the code for you ;)
+
+## Usage
+Any PR page that follows the rule: `https://github.com/*/*/pull/*` for example `https://github.com/PDERAS/edmonton-granite-inventory-web/pull/17` will trigger the script to run. You will know it runs because the "track time" button is overwritten with different text. You will know it fails because I write messages into the DOM on github. This doesn't mess with the track time on the issues page(s).
+
+## Special thanks
+- Brock Roadhouse for some ideas!
